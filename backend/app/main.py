acaea0e9620b3f2d7b8bbdc3e793078355cad4c4
edvale732 <edwardvale73@outlook.com>
2026-09-23 10:34:04 +0100

    only allow vercel deployment as origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://erg-master.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
